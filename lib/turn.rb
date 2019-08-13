b8d5651fd6ae0d463a4e59ca004e4d523531b69f
Jeffrey Musselman <jeffmussels@gmail.com>
2019-08-13 17:59:54 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,5 +6,13 @@ def display_board(board)
  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def input_to_index(user_input)
+  user_input.to_i - 1 
+end
+
+def move(board, index, character='X')
+  board[index] = character
+end
+
 
 
